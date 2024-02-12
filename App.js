fcb3e2ca3b0f073812e4c6eaed0689a7c2db2d93
Pooja Gurung <147177106+Poozagg@users.noreply.github.com>
2024-02-12 18:27:02 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Profile from "./components/Profile"
+import AboutMe from "./components/AboutMe"
+import Footer from "./components/Footer"
+
+
+
+
+export default function App() {
+    return (
+        <div>
+            <Profile />
+            <AboutMe />
+            <Footer />
+          
+        </div>
+    )
+}
